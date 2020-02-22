815a3a61703e816f7a5f3d3f68c15ec014a32b8f
Xavier Perez <xperez007@gmail.com>
2020-02-22 18:10:29 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,7 +4,9 @@ require_relative "../lib/chatty_student.rb"
 class chatty_student
   
 def hello :student, :class
-  puts 
+  puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+end
+
 
 end
 
